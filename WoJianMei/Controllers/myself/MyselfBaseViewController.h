f94b3e62b5564442d2f44150b196a1b5a681a630
Tom Callon <tomcallon2010@gmail.com>
2014-03-08 14:46:45 +0800

    commits

---
+++ WoJianMei/Controllers/myself/MyselfBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyselfBaseViewController.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 2/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyselfBaseViewController : UIViewController
+
+@end
---
+++ WoJianMei/Controllers/myself/MyselfBaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  MyselfBaseViewController.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 2/22/14.
+//
+//
+
+#import "MyselfBaseViewController.h"
+
+@interface MyselfBaseViewController ()
+
+@end
+
+@implementation MyselfBaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
